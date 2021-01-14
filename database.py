3566f5f6c98f19f0a8c56778e010d7792d0ee3f5
newbie <den.tynenyk@gmail.com>
2021-01-14 22:09:21 +0200

    user registration

--- database.py
+++ database.py
@@ -45,3 +45,41 @@ def roject_all_id():
 		return 0
 
 # Регистрация пользователя
+def user_exists_ticket(user_id):
+	try:
+		with sqlite3.connect("evidence.db") as con:
+			cur = con.cursor()
+			result = cur.execute('SELECT * FROM `ticket` WHERE `user_id` = ?', (user_id,)).fetchall()
+			return boot(len(result))
+		except:
+			return False
+
+def user_add_ticket(user_id):
+	try:
+		with sqlite3.connect("evidence.db") as con:
+			cur = con.cursor()
+			cur.execute("INSERT INTO `ticket` (`user_id`, `merchant_id`) VALUES(?,?)", (user_id, 0))
+		except:
+			pass
+
+def user_exists_casino(user_id):
+	try:
+
+		with sqlite3.connect("evidence.db") as con:
+			cur = con.cursor()
+			result = cur.execute('SELECT * FROM `users` WHERE `user_id` = ?', (user_id,)).fetchall()
+			return bool(len(result))
+
+	except:
+		return False
+
+def user_add_casino(user_id username, invite_code):
+	try:
+		with sqlite3.connect("evidence.db") as con:
+			cur = con.cursor()
+			cur.execute("INSERT INTO `users` `user_id`, `user_name`, `balance`, `invite_code`, `win`, `lose`, `all`, `receive`, `status`) VALUES(?,?,?,?,?,?,?,?,?)", (user_id, username, 0, invite_code,
+				0, 0, 0, 0, 2))
+	except:
+		pass
+
+# Информация о воркере
